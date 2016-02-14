feabc1f9f9aca4d9877476679aaaa4fa48cd0e89
maxbbender <maxbbender@gmail.com>
2016-02-14 18:33:54 -0500

    Adding users

--- src/Subfission/Cas/Middleware/CASAuth.php
+++ src/Subfission/Cas/Middleware/CASAuth.php
@@ -2,6 +2,8 @@
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Subfission\Cas\Facades\Cas;
+use App\User;
 
 class CASAuth
 {
@@ -34,6 +36,10 @@ class CASAuth
             // We setup CAS here to reduce the amount of objects we need to build at runtime.  This
             // way, we only create the CAS calls only if the user has not yet authenticated.
             session()->put('cas_user', $this->cas->user());
+        } else {
+            $user = Cas::user();
+            $splitUser = explode($user, "@");
+            $inputUser = User::firstOrCreate(['email' => $user, 'cwid' => $splitUser[0]]);
         }
 
         return $next($request);
